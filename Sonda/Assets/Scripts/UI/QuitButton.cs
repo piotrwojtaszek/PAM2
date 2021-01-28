0cbdab7e60d174924683b2605dea2059a50996b7
Piotr Wojtaszek <48589767+piotrwojtaszek@users.noreply.github.com>
2021-01-28 23:40:19 +0100

    Stable

---
+++ Sonda/Assets/Scripts/UI/QuitButton.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuitButton : MonoBehaviour
+{
+    public void Quit()
+    {
+        StartCoroutine(Wait());
+    }
+    IEnumerator Wait()
+    {
+        yield return new WaitForSeconds(0.5f);
+        Application.Quit();
+    }
+}
